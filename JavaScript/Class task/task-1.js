3a3fe0dfd74f79896b8549f0da4a5a6b4ffe9b12
aneeqanasir <aneeqa.nasir972@gmail.com>
2026-09-15 17:54:08 +0500

    JS Assignment

--- JavaScript/Class task/task-1.js
+++ JavaScript/Class task/task-1.js
@@ -1,1 +1,13 @@
- 
+// for loop
+
+for(var i = 0 ; i < 10 ; i++){
+    console.log(i+1 , "Hellow World");
+}
+var user = +prompt("Enter a number")
+var multiple = +prompt("How many multiple")
+
+user = user || 5
+
+for (var i = 1 ; i <= multiple ; i++){
+    document.write (` ${user} x ${i} = ${i * user} <br>`);
+} 
